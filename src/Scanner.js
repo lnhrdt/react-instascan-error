59359ae1982eb6f07ec1f5c69805ce3848b4fda0
Leonhardt Koepsell <leonhardt@koepsell.io>
2018-06-11 16:06:44 -0500

    Remove react-instascan and simplify example of error

--- src/Scanner.js
+++ src/Scanner.js
@@ -1,14 +1,7 @@
 import React from 'react';
-import {Cameras, Scanner} from "react-instascan";
+import {Camera} from "instascan";
 
-export default () =>
-    <Cameras>
-        {cameras => (
-            <div>
-                <h1>Scan the code!</h1>
-                <Scanner camera={cameras[0]} onScan={console.log}>
-                    <video style={{width: 400, height: 400}}/>
-                </Scanner>
-            </div>
-        )}
-    </Cameras>
+export default () => {
+    Camera.getCameras().then(cameras => console.log(cameras));
+    return <div/>;
+}
